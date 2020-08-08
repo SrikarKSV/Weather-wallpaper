0e7f02415034f8810e8c581bc3f2219075062667
Srikar KSV <srikar1awesome@gmail.com>
2020-08-08 11:18:09 +0530

    Now of there is a error❌ while fetching images then Default images will be set as background.

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -8,34 +8,39 @@ ABSOLUTE_DIR = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(ABSOLUTE_DIR)
 
 
-def download_wallpaper(query):
-    headers = {"Accept-Version": "v1"}
+def download_wallpaper(main, description):
+    try:
+        headers = {"Accept-Version": "v1"}
 
-    api_params = {
-        'page': random.randint(1, 3),
-        'query': query,
-        'orientation': 'landscape',
-        "client_id": os.getenv('UNSPLASH_API')
-    }
+        api_params = {
+            'query': description,
+            'orientation': 'landscape',
+            "client_id": os.getenv('UNSPLASH_API')
+        }
 
-    r = requests.get(
-        'https://api.unsplash.com/search/photos',
-        params=api_params,
-        headers=headers
-    )
+        r = requests.get(
+            'https://api.unsplash.com/search/photos',
+            params=api_params,
+            headers=headers
+        )
 
-    response = r.json()['results']
-    pic = response[random.randint(0, 10)]['urls']['raw']
+        response = r.json()['results']
+        pic = response[random.randint(0, 10)]['urls']['raw']
 
-    print(pic)
-    with requests.get(pic, stream=True) as picture, open(f'{BASE_DIR}/downloads/wallpaper.jpg', 'wb') as f:
-        f.write(picture.content)
+        print(pic)
+        with requests.get(pic, stream=True) as picture, open(f'{BASE_DIR}/downloads/wallpaper.jpg', 'wb') as f:
+            f.write(picture.content)
 
-    ctypes.windll.user32.SystemParametersInfoW(
-        20, 0, os.path.join(BASE_DIR, 'downloads/wallpaper.jpg'), 3)
-
+        ctypes.windll.user32.SystemParametersInfoW(
+            20, 0, os.path.join(BASE_DIR, 'downloads/wallpaper.jpg'), 3)
+    except:
+        DEFAULT_IMAGES_FOLDER = os.path.join(BASE_DIR, "Default images", main)
+        images_list = os.listdir(DEFAULT_IMAGES_FOLDER)
+        random_image = images_list[random.randint(0, len(images_list) - 1)]
+        ctypes.windll.user32.SystemParametersInfoW(
+            20, 0, os.path.join(DEFAULT_IMAGES_FOLDER, random_image), 3)
 
 
 if __name__ == "__main__":
-    download_wallpaper('freezing rain')
+    download_wallpaper('Fog', 'damn')
     print('Done')
